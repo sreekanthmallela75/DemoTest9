f93af25ec8f2d08fe35f80ca3aa090ced1e7ff18
sreekanthmallela75 <sreekanthmallela75@gmail.com>
2023-01-06 18:30:05 +0530

    Initial commit

---
+++ src/com/stringReplcement/StringtReplcement.java
@@ -0,0 +1,13 @@
+package com.stringReplcement;
+
+public class StringtReplcement {
+
+    public static void main(String[] args) {
+        String sname ="Sreekanth       Mallela";
+        String replacement = sname.replaceAll("s" , "");
+        System.out.println(replacement);
+        //System.out.println(sname.trim());
+         //String replaceString = sname.replace('e','t');
+        //System.out.println(replaceString);
+    }
+}
